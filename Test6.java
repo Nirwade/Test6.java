e9ee034e3b6818c5fab293be7d6ea6250cb91805
Nirwade <nirwadeshashi@gmail.com>
2022-03-06 17:10:19 +0530

    first commit

---
+++ Test6.java
@@ -0,0 +1,32 @@
+package Test3;
+
+public class Test6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1=20;
+		int num2=30;
+		int num3=40;
+		//multiplication
+		//Division
+		int sum=num1+num2;
+		int muli=num2*num3;
+		float div= (float)num1/num2;
+		String result = "sum of two numbers is " + sum;
+		String result1 = "mul of two numbers is " + muli;
+		String result2 = "div of two numbers is " + div;
+
+		System.out.println(result);
+		System.out.println(result1);
+		System.out.println(result2);
+		
+		String name  = "This is just an example";
+				System.out.println(name.length());
+				
+				char char1 = name.charAt(0);
+				System.out.println(char1);
+
+	}
+
+}
